114feaf68bd2133f27ab6620ee1fe50ae515e82e
Ziad Zebib <zebibziad@gmail.com>
2020-12-29 00:55:14 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,6 +38,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  
+  assembled_matrix[0][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
